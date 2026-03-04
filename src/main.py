fe5c5cc6cdab505bd8e080f7b5cd15d6d72aba4b
Yonilee26 <yonilee26@gmail.com>
2026-03-04 14:50:55 -0700

    convert script to proper CLI tool

--- src/main.py
+++ src/main.py
@@ -1,5 +1,31 @@
+import argparse
+
+
+def greet(name: str):
+    print(f"Hello, {name}!")
+
+
 def main():
-    print("Hello from the main program")
+    parser = argparse.ArgumentParser(
+        prog="greet",
+        description="A simple CLI tool that greets a user"
+    )
+
+    parser.add_argument(
+        "name",
+        help="Name of the person to greet"
+    )
+
+    parser.add_argument(
+        "--version",
+        action="version",
+        version="greet 1.0"
+    )
+
+    args = parser.parse_args()
+
+    greet(args.name)
+
 
 if __name__ == "__main__":
     main()
